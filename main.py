427cd670242f720f0516adfe2d84a4522c0aecbe
DESKTOP-3FJQRQS\Asus <dunlez10@gmail.com>
2020-12-15 11:20:03 -0500

    1

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ mi_app = FastAPI()
 
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-    "http://localhost", "http://localhost:8080", "https://monit-cajero-api.herokuapp.com/"
+    "http://localhost", "http://localhost:8080", "https://monit-cajero-app.herokuapp.com"
 ]
 mi_app.add_middleware(
     CORSMiddleware, allow_origins=origins,
